0ec01a51391dfead7c4873550e24a2c076e8358e
Gustavo Carvalho <guga.a.carvalho@gmail.com>
2019-05-07 08:24:36 -0300

    Fix map integrator and add example

--- src/normalMapIntegrator.cpp
+++ src/normalMapIntegrator.cpp
@@ -1,4 +1,5 @@
 #include "normalMapIntegrator.h"
+#include <iostream>
 #include "surfaceInteraction.h"
 #include "vec3.h"
 
@@ -8,9 +9,11 @@ Pixel NormalMapIntegrator::Li(const Ray& ray, const Scene& scene,
   if (!scene.intersect(ray, &isect)) {
     // This might be just:
     return scene.background->getColor(ray.getI(), ray.getJ());
-
-    isect.n = unit_vector(isect.n);
-    vec3 modifiedN =
-        0.5 * vec3(isect.n.x() + 1, isect.n.y() + 1, isect.n.z() + 1);
-    return Pixel(255 * modifiedN.x(), 255 * modifiedN.y(), 255 * modifiedN.z());
   }
+
+  isect.n = unit_vector(isect.n);
+  vec3 modifiedN =
+      0.5 * vec3(isect.n.x() + 1, isect.n.y() + 1, isect.n.z() + 1);
+
+  return Pixel(255 * modifiedN.x(), 255 * modifiedN.y(), 255 * modifiedN.z());
+}
